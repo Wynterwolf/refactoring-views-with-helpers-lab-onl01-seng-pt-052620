b43905995b13acc96023c755351266c75ed49ce2
wynterwolf <shinyguns@gmail.com>
2020-10-24 21:48:21 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,14 @@
 module ArtistsHelper
+
+    # Write a helper method #display_artist in the appropriate app/helpers file to be called on in our views. 
+    # The method's return value should take into consideration whether an artist is already associated with a song
+    
+    def display_artist(song)
+        if song.artist
+            link_to song.artist.name, song.artist
+        else
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
+
 end
